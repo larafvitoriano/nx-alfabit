c7fe8d00c88d21cebe2d3bb678948ed09b16cfab
Lara Fernanda <laraalmeida@tce.rn.gov.br>
2025-07-16 09:59:34 -0300

    feat: adicionando login page

---
+++ libs/login-page/src/lib/login-page/login-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ab-login-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css',
+})
+export class LoginPageComponent {}
