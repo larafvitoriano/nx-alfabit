5105c51ffccb93a9d0f9b96019443e742c63d8a2
Lara Fernanda <laraalmeida@tce.rn.gov.br>
2025-07-16 10:26:16 -0300

    feat: login page

--- apps/alfabit/src/app/app.component.ts
+++ apps/alfabit/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { ButtonComponent } from '@lara/button';
+import { LoginPageComponent } from '@lara/login-page';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, ButtonComponent],
+  imports: [NxWelcomeComponent, RouterModule, ButtonComponent, LoginPageComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
---
+++ libs/storybook-host/src/stories/login-page.component.stories.ts
@@ -0,0 +1,10 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { LoginPageComponent } from '@lara/login-page';
+
+const meta: Meta<LoginPageComponent> = {
+  component: LoginPageComponent,
+};
+
+export default meta;
+
+type Story = StoryObj<LoginPageComponent>;
