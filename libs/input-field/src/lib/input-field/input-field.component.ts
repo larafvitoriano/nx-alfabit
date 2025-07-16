deb11ba068ed32d9d26d97a2cbd1343be59be45d
Lara Fernanda <laraalmeida@tce.rn.gov.br>
2025-07-16 10:29:41 -0300

    feat: adicionando input-field

---
+++ libs/input-field/src/lib/input-field/input-field.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'tce-input-field',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './input-field.component.html',
+  styleUrl: './input-field.component.css',
+})
+export class InputFieldComponent {}
