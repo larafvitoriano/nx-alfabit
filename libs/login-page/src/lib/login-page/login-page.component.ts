39c8fe7d8b504e7e7434c0f21a695a269adeffe9
Lara Fernanda <laraalmeida@tce.rn.gov.br>
2025-07-17 07:29:27 -0300

    feat: estilizando login page

--- libs/login-page/src/lib/login-page/login-page.component.ts
+++ libs/login-page/src/lib/login-page/login-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputFieldComponent } from '@lara/input-field';
+import { ButtonComponent } from '@lara/button';
 
 @Component({
   selector: 'ab-login-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, InputFieldComponent, ButtonComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css',
 })
---
+++ libs/storybook-host/src/stories/login-page.component.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { LoginPageComponent } from '@lara/login-page';
+import { moduleMetadata } from '@storybook/angular';
+
+import { InputFieldComponent } from '@lara/input-field';
+import { ButtonComponent } from '@lara/button';
+
+const meta: Meta<LoginPageComponent> = {
+  title: 'Pages/LoginPage',
+  component: LoginPageComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [LoginPageComponent, InputFieldComponent, ButtonComponent],
+    }),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<LoginPageComponent>;
+
+export const Default: Story = {};
